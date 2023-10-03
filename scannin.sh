2064915725d1b75c8426a6d535271c492727bffb
Flavio Silva <flavio.silvapinkman@gmail.com>
2023-10-03 17:05:50 -0300

    Update scannin.sh

--- scannin.sh
+++ scannin.sh
@@ -9,8 +9,25 @@ read doors
 echo "TIMING 0-5 [+]: "
 read timing
 
-echo "Aguarde, a consulta sera feita em instantes."
 
-scan=sudo nmap $target -p$doors -T$timing
 
-echo scan
+
+
+
+echo "Qual scanning voce deseja fazer (1 - TCP, 2 - UDP,3 - Servicos, 4 = SO)"
+read choice
+
+if [[ choice -eq 1 ]]; then
+	scanTCP= sudo nmap -T$timing -sS $target -p$doors
+  echo scanTCP
+elif [[ choice -eq 2 ]]; then
+  scanUDP= sudo nmap -T$timing -sU $target -p$doors
+  echo scanUDP
+elif [[ choice -eq 3 ]]; then
+  scanService= sudo nmap -T$timing -sU $target -p$doors
+  echo scanService
+elif [[ choice -eq 4 ]]; then
+	scanSO= sudo nmap -T$timing -sO $target -p$doors
+	echo scanSO	
+fi
+
